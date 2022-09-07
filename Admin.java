730dbc687f4f46fedde864ddbcfd99729d8b1d5c
atchayaraj <72649598+atchayaraj@users.noreply.github.com>
2022-09-07 16:37:58 +0530

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,10 @@
+class Admin {
+	private String username = "Admin2022";
+	private String password = "Admin10";
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
